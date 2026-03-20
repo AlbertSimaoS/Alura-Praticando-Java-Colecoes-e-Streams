b52ca3951290c39044ea5c0b0ebb086a0cebc781
Albert <72774907+AlbertSimaoS@users.noreply.github.com>
2026-03-20 11:26:05 -0300

    Create Main.java

---
+++ Exercicios/Exc1/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> EmployeesList = new ArrayList<>();
+
+        EmployeesList.add("João");
+        EmployeesList.add("Maria");
+        EmployeesList.add("Vitor");
+        EmployeesList.add("Ana");
+
+        System.out.printf("Lista de funcioários: " + EmployeesList);
+
+    }
+}
